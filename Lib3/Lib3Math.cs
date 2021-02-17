c783c5de7221dc088e1ec27d9c515f054cdcb14e
Carlos Leite <CarlosLeite@criticalmanufacturing.com>
2021-02-17 15:54:58 +0000

    init commit

---
+++ Lib3/Lib3Math.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib3
+{
+    public static class Lib3Math
+    {
+        public static int Plus(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
